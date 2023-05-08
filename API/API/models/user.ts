0f0450885c387be41589b8ea4040e39280167d60
ThiagoLeto <81710270+ThiagoLeto@users.noreply.github.com>
2023-05-08 13:29:21 -0300

    Delete API directory

--- API/API/models/user.ts
+++
@@ -1,9 +0,0 @@
-export class user{
-    nombre:string;
-    hash:string;
-    salt:string;
-
-    constructor(InNombre:string,InHash:string,salt:string){ 
-        this.nombre=InNombre ; this.hash = InHash ; this.salt = salt;
-    }
-}
